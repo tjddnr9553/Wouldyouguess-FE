00023ba748e93d32e41f769b97e016fea2a3718a
Chaeyun06 <kilskg@gmail.com>
2024-07-08 20:27:29 +0900

    login

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,59 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Home.css'
 import { KAKAO_AUTH_URL } from '../../api/oauth/Oauth';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const kakaoLoginURL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=6484bad6b0521f3[…]86&redirect_uri=http://localhost:8080/login/oauth2/code/kakao"
+let url = 'https://localhost:8080';
+const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=6484bad6b0521f31f7fca78c30752386&redirect_uri=${url}/login/oauth2/code/kakao`
+
+// const loginRequest = 'http://localhost:8080/loginSuccess';
+
+
 
 const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const nav = useNavigate();
 
-  // login 상태에 따른 화면 렌더링 상태를 변경하는 코드를 추가해야 함.
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const getLoginData = async () => {
+    const response = await axios.get('/loginSuccess');
 
-  const loginOrCreateRoom = () => {
+    const { nickname } = response.data;
 
-  }
+    if (!nickname) {
+      nav("/lobby");
+    } else {
+      nav("/");
+      }
+  };
+
+
+  const goToKakaoLogin = () => {
+    window.location.href = kakaoLoginURL;
+    
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    getLoginData();
+  }, [isLoggedIn])
+
+  useEffect(() => {
+    getLoginData();
+  }, [])
 
 
   return (
     <div className="home background">
       { !isLoggedIn ? 
       (
-        <button className="img-button" onClick={() => { nav(kakaoLoginURL) }}>
+        <button className="img-button" onClick={goToKakaoLogin}>
           <img src='/images/btn/basic_btn.png' alt="btn" className="btn-img" />
           <div className="btn-start">START</div>
         </button>
       ) : 
       (
-      <button className="img-button" onClick={() => { nav(kakaoLoginURL) }}>
+      <button className="img-button" onClick={goToKakaoLogin}>
         <img src='/images/btn/basic_btn.png' alt="btn" className="btn-img" />
         <div className="btn-start">START</div>
       </button>
