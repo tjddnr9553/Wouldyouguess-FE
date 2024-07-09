41d5beb72a53061789c52efff790b675a45b2977
tjddnr9553 <tjddnr9553@naver.com>
2024-07-09 20:52:23 +0900

    # temp

--- src/App.jsx
+++ src/App.jsx
@@ -22,36 +22,36 @@ import Profile from "./pages/home/Profile.jsx";
 import LoginHandler from "./pages/oauth/LoginHandler.jsx";
 
 function App() {
-  const REDIRECT_URL = '/auth/kakao/callback';
+    const REDIRECT_URL = '/auth/kakao/callback';
 
-  return (
-    <>
-      <GlobalStyles />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/login' element={<Login />} />
-          <Route path="/lobby/:roomUrl" element={<Lobby />} />
+    return (
+        <>
+            <GlobalStyles />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/login' element={<Login />} />
+                <Route path="/lobby/:roomUrl" element={<Lobby />} />
 
-        <Route path='/game1' element={<Game1 />} />
-        <Route path='/test' element={<TempCanvas />} />
-        <Route path='/game1/vote' element={<Game1_vote />} />
-        <Route path='/game1/result' element={<Game1_result />} />
+                <Route path='/game1' element={<Game1 />} />
+                <Route path='/test' element={<TempCanvas />} />
+                <Route path='/game1/vote' element={<Game1_vote />} />
+                <Route path='/game1/result' element={<Game1_result />} />
 
-        <Route path="/game2" element={<Game2 />} />
-        <Route path="/game2/upload" element={<Game2_upload />} />
-        <Route path="/game2/remember" element={<Game2_remember />} />
-        <Route path="/game2/result" element={<Game2_result />} />
+                <Route path="/game2" element={<Game2 />} />
+                <Route path="/game2/upload" element={<Game2_upload />} />
+                <Route path="/game2/remember" element={<Game2_remember />} />
+                <Route path="/game2/result" element={<Game2_result />} />
 
-        <Route path='/game3' element={<Game3 />} />
-        <Route path='/result' element={<Result />} />
-        <Route path='/loading' element={<Loading />} />
-        <Route path='/temp' element={<Temp />} />
+                <Route path='/game3' element={<Game3 />} />
+                <Route path='/result' element={<Result />} />
+                <Route path='/loading' element={<Loading />} />
+                <Route path='/temp' element={<Temp />} />
 
-        <Route path={REDIRECT_URL} element={<LoginHandler />} />
-      </Routes>
-    </>
-  );
+                <Route path={REDIRECT_URL} element={<LoginHandler />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
