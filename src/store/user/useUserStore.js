3ba4d1e33fd16ae93d09f866455852e3fb2ee4dc
박현민 <dndb4902@naver.com>
2024-07-10 18:30:12 +0900

    # api & login & nickname settings

--- src/store/user/useUserStore.js
+++ src/store/user/useUserStore.js
@@ -1,21 +1,15 @@
 import { create } from "zustand";
 
 const useUserStore = create((set) => ({
-  userId: null,
-  isLogin: false,
-  isInvited: false,
-  participateRoomId: 0,
-  nickname: "김광윤",
-  username: "김광윤",
+  userId: 0,
+  nickname: "",
+  username: "",
   accessToken: "",
   rank: 0,
   score: 0,
   gameRole: "drawer", // drawer, watcher
 
   setUserId: (userId) => set({ userId }),
-  setIsLogin: (isLogin) => set({ isLogin }),
-  setIsInvited: (isInvited) => set({ isInvited }),
-  setParticipateRoomId: (participateRoomId) => set({ participateRoomId }),
   setNickname: (nickname) => set({ nickname }),
   setUsername: (username) => set({ username }),
   setAccessToken: (accessToken) => set({ accessToken }),
