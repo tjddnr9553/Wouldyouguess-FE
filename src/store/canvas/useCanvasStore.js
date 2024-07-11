52c30d05a6286128e90d30f10e6d95cee6a40235
Chaeyun06 <kilskg@gmail.com>
2024-07-11 22:14:41 +0900

    modify

--- src/store/canvas/useCanvasStore.js
+++ src/store/canvas/useCanvasStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 
-export const useCanvasStore = create((set) => ({
+export const useCanvasStore = create((set, get) => ({
     tool: 'pencil',
     setTool: (tool) => set({ tool }),
     color: '#000',
@@ -9,4 +9,13 @@ export const useCanvasStore = create((set) => ({
     setSize: (size) => set({ size }),
     fillColor: '',
     setFillColor: (fillColor) => set({ fillColor }),
+    clearBtnClick: false,
+    setClearBtnClick: (clearBtnClick) => set({ clearBtnClick }),
+    canvas: '',
+    setCanvas: (canvas) => set({canvas}),
+    getSaveImg: () => {
+      const canvas = get().canvas;
+      const imgUrl = canvas.toDataURL("image/png");
+      return imgUrl;
+    },
   }));
