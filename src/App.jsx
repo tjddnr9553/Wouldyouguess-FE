0a46138f0fd106d695bdffcb1f2a33b91fa2e608
박현민 <dndb4902@naver.com>
2024-07-13 14:10:27 +0900

    # 초대 시 페이지 이동 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import {useEffect} from "react";
 import "./App.css";
@@ -21,6 +21,8 @@ import LoginHandler from "./api/oauth/LoginHandler.jsx";
 import {KAKAO_AUTH_URL} from "./api/oauth/Oauth.js";
 
 function App() {
+  const nav = useNavigate();
+
   // 초대 받은 사람은 여기서 분류
   useEffect(() => {
     const currentUrl = new URL(window.location.href);
@@ -31,7 +33,8 @@ function App() {
       window.localStorage.setItem("inviteRoomId", inviteRoomId);
       window.localStorage.setItem("isInvited", "true");
       console.log("초대받은 주소 : ", inviteRoomId);
-      window.location.href = KAKAO_AUTH_URL;
+      nav('/temp/login');
+      // window.location.href = KAKAO_AUTH_URL;
     }
   }, []);
 
