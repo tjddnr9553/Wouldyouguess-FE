c02841e93df2c6726a2a7fd354340d7a007c6844
Chaeyun06 <kilskg@gmail.com>
2024-07-20 22:24:28 +0900

    게이지 바 완성, 5초 후 동작함

---
+++ src/pages/game1/Gaugebar.jsx
@@ -0,0 +1,13 @@
+import './Gaugebar.css'
+
+const Gaugebar = () => {
+  return (
+    <div className="gaugebar">
+      <div className="loader">
+        <img src="/images/game/rocket.svg" className='rocket'/>
+      </div>
+    </div>
+  )
+}
+
+export default Gaugebar;
