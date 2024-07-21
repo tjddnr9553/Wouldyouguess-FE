b82195448eee0182e34b340c7ad1eba9678306b0
Chaeyun06 <kilskg@gmail.com>
2024-07-21 15:29:18 +0900

    캔버스 크기 고정

--- src/pages/game1/Game1.jsx
+++ src/pages/game1/Game1.jsx
@@ -17,6 +17,8 @@ import useAudioStore from "../../store/bgm/useAudioStore.js";
 import Keyword from "../../components/game/Keyword.jsx";
 import KeywordText from "../../components/game/KeywordText.jsx";
 import LaserPointer from "./LaserPointer.jsx";
+import Gaugebar from "./Gaugebar.jsx";
+import Test from "../Test.jsx";
 
 const Game1 = () => {
   const [searchParams] = useSearchParams();
@@ -28,7 +30,7 @@ const Game1 = () => {
   const [parentheight, setParentHeight] = useState(0);
   const [waitText, setWaitText] = useState(null);
   const [showModal, setShowModal] = useState(true); // 모달 표시 상태
-  const [clockStart, setClockStart] = useState(false); // 시계 시작 상태
+  const [gameStart, setGameStart] = useState(false); // 게임 시작 상태, 30초 시작
   const [titleOn, setTitleOn] = useState(false);
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
@@ -74,7 +76,7 @@ const Game1 = () => {
       setWaitText("다른 플레이어의 차례");
     }
 
-    setClockStart(false);
+    setGameStart(false);
     setShowModal(true);
     setTitleOn(false);
 
@@ -85,61 +87,47 @@ const Game1 = () => {
     // 5초 후에 모달 닫고 시계 시작
     const timer = setTimeout(() => {
       setShowModal(false);
-      setClockStart(true); // 시계 시작 상태 변경
+      setGameStart(true); // 게임 시작 상태 변경, 게임 start
     }, 6000);
     return () => clearTimeout(timer);
   }, [round]);
 
-  // window size 변경 시 캔버스 좌표 수정을 위한 resize
-  useEffect(() => {
-    window.addEventListener("resize", () =>
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      })
-    );
-    setParentWidth(containerRef.current.clientWidth);
-    setParentHeight(containerRef.current.clientHeight);
-  }, [windowSize]);
-
   return (
-    <div className="inner" key={round}>
-      {showModal && isDrawing && <Keyword keyword={keyword} />}
-      {showModal && !isDrawing && <Keyword keyword={"다른 플레이어의 차례"} />}
-      <div className="game container">
-        <div className="left-section">
-          <User />
-        </div>
-        <div className="center">
-          <div className="keyword-title">
-            {titleOn && isDrawing && <KeywordText text={keyword} />}
-            {titleOn && !isDrawing && (
-              <KeywordText text={"다른 플레이어의 차례"} />
-            )}
-            {/* <hr /> */}
+    <div className="mainwrap" >
+      <Test />
+      <div className="inner" key={round}>
+        {showModal && isDrawing && <Keyword keyword={keyword} />}
+        {showModal && !isDrawing && <Keyword keyword={"다른 플레이어의 차례"} />}
+        <div className="game container">
+          <div className="left-section">
+            <User />
           </div>
-          <div className="drawing-container">
-            <div ref={containerRef} className="canvas-container">
-              <Drawing
-                width={parentwidth * 0.9}
-                height={parentheight * 0.9}
-                zIndex={isDrawing ? 10 : 9}
-                position={isDrawing ? "absolute" : "relative"}
-              />
-              <LaserPointer
-                width={parentwidth * 0.9}
-                height={parentheight * 0.9}
-                zIndex={!isDrawing ? 10 : 9}
-                position={!isDrawing ? "absolute" : "relative"}
-              />
+          <div className="center">
+            <Gaugebar gameStart = {gameStart}/>
+            <div className="keyword-title">
+              {titleOn && isDrawing && <KeywordText text={keyword} />}
+              {titleOn && !isDrawing && (
+                <KeywordText text={"다른 플레이어의 차례"} />
+              )}
+              {/* <hr /> */}
+            </div>
+            <div className="drawing-container">
+              <div ref={containerRef} className="canvas-container">
+                <Drawing
+                  zIndex={isDrawing ? 10 : 9}
+                  position={isDrawing ? "absolute" : "relative"}
+                />
+                <LaserPointer
+                  zIndex={!isDrawing ? 10 : 9}
+                  position={!isDrawing ? "absolute" : "relative"}
+                />
+              </div>
             </div>
           </div>
-          <div className="canvas-tools">
+          <div className="right-section">
+            <Tools />
+            <Palette />
           </div>
-        </div>
-        <div className="right-section">
-          <Tools />
-          <Palette />
         </div>
       </div>
     </div>
