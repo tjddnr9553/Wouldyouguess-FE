7f7f364c3ea2715bf10757d660813689db5bf289
박현민 <dndb4902@naver.com>
2024-07-26 19:56:53 +0900

    # deploy test

--- src/pages/game2/Video.tsx
+++ src/pages/game2/Video.tsx
@@ -37,6 +37,7 @@ function Video({
   const [isWinner, setIsWinner] = useState(null);
 
   useEffect(() => {
+    setIsWinner(null);
     const sync_func = async () => {
       const res = await catchLiar_result(gameId, userId);
 
@@ -93,22 +94,9 @@ function Video({
     const delay = 1.5;
     const id = "camera-" + participantIdentity;
     floatingObj(id, delay);
-    players.map((player) => {
-      if(player.isWinner === false) {
-        gsap.to('.loser', {
-          duration: 1,
-          delay: 3.5,
-          x: 1000,
-          y: -300,
-          rotateX: 360,
-          rotationY: 360,
-          rotationZ: 360,
-          opacity: 0,
-          ease: "pwer1.easeInOut"
-        })
-      }
-    })
-    // if (isWinner === false && isWinner !== null) {
+
+    // players.map((player) => {
+    //   if(player.isWinner === false) {
     //     gsap.to('.loser', {
     //       duration: 1,
     //       delay: 3.5,
@@ -120,7 +108,21 @@ function Video({
     //       opacity: 0,
     //       ease: "pwer1.easeInOut"
     //     })
-    // }
+    //   }
+    // })
+    if (isWinner === false && isWinner !== null) {
+        gsap.to('.loser', {
+          duration: 1,
+          delay: 3.5,
+          x: 1000,
+          y: -300,
+          rotateX: 360,
+          rotationY: 360,
+          rotationZ: 360,
+          opacity: 0,
+          ease: "pwer1.easeInOut"
+        })
+    }
     // if (isWinner === true && isWinner !== null) {
     //   gsap.to(".winner", {
     //     duration: 1,    
@@ -131,12 +133,12 @@ function Video({
     //     ease: "power1.inOut" // 애니메이션의 easing 함수
     //   });
     // }
-  }, [players])
+  }, [isWinner])
 
 
 
   return (
-    <div id={"camera-" + participantIdentity} className={`${classNameCss ? classNameCss : ''} ${isWinner ? 'winner' : 'loser'}`}
+    <div id={"camera-" + participantIdentity} className={`${classNameCss ? classNameCss : ''} ${isWinner && isWinner ? 'winner' : 'loser'}`}
     style={{position: 'relative'}}>
       <div className="ufo-nick">
         {currentPlayer && 
